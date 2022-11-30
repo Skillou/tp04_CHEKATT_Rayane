c228dda06ede6fc012754246c1fd71f1f8e3278b
Skillou <75482995+Skillou@users.noreply.github.com>
2022-11-30 18:13:46 +0100

    TP4 : Nouvelle recherche

--- FRONTEND/src/app/catalog/recherche-produits/recherche-produits.component.ts
+++ FRONTEND/src/app/catalog/recherche-produits/recherche-produits.component.ts
@@ -1,14 +1,18 @@
 import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from "@angular/forms";
 import { RechercheProduitsService } from "../../Utils/Services/recherche-produits.service";
+import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
 
 type Category = 'Livre' | 'Manga' | 'Jeu';
+
+@UntilDestroy()
 
 @Component({
   selector: 'app-recherche-produits',
   templateUrl: './recherche-produits.component.html',
   styleUrls: ['./recherche-produits.component.css']
 })
+
 export class RechercheProduitsComponent {
 
   protected readonly searchGroup = inject(FormBuilder).nonNullable.group({
@@ -21,11 +25,12 @@ export class RechercheProduitsComponent {
   })
 
   constructor(private readonly fb: FormBuilder, private readonly service: RechercheProduitsService) {
-    
+    this.searchGroup.controls.search.valueChanges.pipe(untilDestroyed(this)).subscribe(search => this.service.searchProducts(search));
+    // this.searchGroup.controls.category.valueChanges.pipe(untilDestroyed(this)).subscribe(category => this.service.categoryProducts(category);
   }
 
   public search(): void {
-    // this.service.searchProducts(this.searchControl.value);
+    // this.service.searchProducts(this.searchGroup.controls.search.value);
   }
 
   public onChangeCategory(category: Category) {
