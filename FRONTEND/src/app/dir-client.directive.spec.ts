bf130edeb63f8eeddc6e7212e59700925a9c2236
Skillou <75482995+Skillou@users.noreply.github.com>
2022-12-02 13:37:23 +0100

    ifhezfhzeiulfhzihefuzhfoziuhfuifzeofuzefgzefuizhfuizhfizhfiuzhfuizhfoiuzhfoiuzehfuizhfozeifhzeiuhfziufhzpuifhz

--- FRONTEND/src/app/dir-client.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirClientDirective } from './dir-client.directive';
-
-describe('DirClientDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirClientDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- FRONTEND/src/app/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    produits: '/assets/mock/produits.json',
-    name: 'default',
-  };
